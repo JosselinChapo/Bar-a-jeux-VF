a9b4a36b87583dbf89f60e397e32ede446716751
LanselleD <72977738+LanselleD@users.noreply.github.com>
2023-02-24 16:26:47 +0100

    fin CRUD client

--- front-bar-a-jeux/src/app/client/client.service.ts
+++ front-bar-a-jeux/src/app/client/client.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Client } from '../model';
 
 @Injectable({
@@ -7,28 +9,54 @@ import { Client } from '../model';
 export class ClientService {
 
   clients: Array<Client> = new Array<Client>();
-  civilites: Map<string, string> = new Map<string,string>();
+  civilites: Array<string> = new Array<string>();
 
   constructor(private http: HttpClient) { 
     this.load();
     this.loadCivilites();
   }
 
+  findAllCivilite(): Array<string> {
+    return this.civilites;
+  }
 
+  findAll(): Array<Client> {
+    return this.clients;
+  }
 
+  findById(id: number): Observable<Client> {
+    return this.http.get<Client>("http://localhost:8888/client/" + id);
+  }
 
+  create(client: Client): void {
+    this.http.post<Client>("http://localhost:8888/client", client).subscribe(resp => {
+      this.load();
+    });
+  }
+
+  update(client: Client): void {
+    this.http.put<Client>("http://localhost:8888/client/" + client.id, client).subscribe(resp => {
+      this.load();
+    });
+  }
+
+  remove(id: number): void {
+    this.http.delete<void>("http://localhost:8888/client/" + id).subscribe(resp => {
+      this.load();
+    });
+  }
 
 
   private load(): void {
-    this.http.get<Array<Stagiaire>>("http://localhost:8888/stagiaire").subscribe(resp => {
-      this.stagiaires = resp;
+    this.http.get<Array<Client>>("http://localhost:8888/client").subscribe(resp => {
+      this.clients = resp;
     });
   }
 
   private loadCivilites(): void {
     this.http.get<Array<string>>("http://localhost:8888/civilites").subscribe(resp => {
       resp.forEach(civ => {
-        this.civilites.set(civ, civ);
+        this.civilites;
       });
     });
   }
