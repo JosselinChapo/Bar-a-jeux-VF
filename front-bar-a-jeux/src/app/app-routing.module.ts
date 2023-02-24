4368801f3d037bf7e4a126cd6798c1bbe46f6334
Maximilien <maximilien.blanc@hotmail.fr>
2023-02-24 14:35:34 +0100

    Init Component

--- front-bar-a-jeux/src/app/app-routing.module.ts
+++ front-bar-a-jeux/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { AdminComponent } from './admin/admin.component';
+import { ClientComponent } from './client/client.component';
+import { CollectionComponent } from './collection/collection.component';
+import { ReservationComponent } from './reservation/reservation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: AccueilComponent, pathMatch: 'full'},
+  {path: "reservation", component: ReservationComponent},
+  {path: "collection", component: CollectionComponent},
+  {path: "client", component: ClientComponent},
+  {path: "admin", component: AdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-bar-a-jeux/src/app/app.module.ts
+++ front-bar-a-jeux/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { CollectionComponent } from './collection/collection.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { ClientComponent } from './client/client.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    CollectionComponent,
+    ReservationComponent,
+    ClientComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
