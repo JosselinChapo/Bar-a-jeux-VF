1a6f39e9642dec5be6ae45fc4168e6cbfe8d27eb
LanselleD <72977738+LanselleD@users.noreply.github.com>
2023-02-24 16:27:13 +0100

    fin CRUD component client

--- front-bar-a-jeux/src/app/client/client.component.ts
+++ front-bar-a-jeux/src/app/client/client.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Client } from '../model';
+import { ClientService } from './client.service';
 
 @Component({
   selector: 'app-client',
@@ -6,12 +8,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./client.component.css']
 })
 export class ClientComponent {
+  formClient: Client = null;
 
+  constructor(private clientService: ClientService) {
+  }
 
+  list(): Array<Client> {
+    return this.clientService.findAll();
+  }
 
+  listCivilites(): Array<string> {
+    return this.clientService.findAllCivilite();
+  }
 
+  add(): void {
+    this.formClient = new Client();
 
-  
+  }
 
+  edit(id: number): void {
+    this.clientService.findById(id).subscribe(response => {
+      this.formClient = response;
+    });
+  }
+
+  save(): void {
+    if(this.formClient.id) { // UPDATE
+      this.clientService.update(this.formClient);
+    } else { // CREATE
+      this.clientService.create(this.formClient);
+    }
+
+    this.cancel();
+  }
+
+  remove(id: number): void {
+    this.clientService.remove(id);
+  }
+
+  cancel(): void {
+    this.formClient = null;
+  }
   
 }
