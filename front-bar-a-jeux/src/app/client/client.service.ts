bbd68d01b79aafcec918c4b6ffd8535d3d2ebde9
LanselleD <72977738+LanselleD@users.noreply.github.com>
2023-02-24 16:03:25 +0100

    début écriture service client

--- front-bar-a-jeux/src/app/client/client.service.ts
+++ front-bar-a-jeux/src/app/client/client.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Client } from '../model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  clients: Array<Client> = new Array<Client>();
+  civilites: Map<string, string> = new Map<string,string>();
+
+  constructor(private http: HttpClient) { 
+    this.load();
+    this.loadCivilites();
+  }
+
+
+
+
+
+
+  private load(): void {
+    this.http.get<Array<Stagiaire>>("http://localhost:8888/stagiaire").subscribe(resp => {
+      this.stagiaires = resp;
+    });
+  }
+
+  private loadCivilites(): void {
+    this.http.get<Array<string>>("http://localhost:8888/civilites").subscribe(resp => {
+      resp.forEach(civ => {
+        this.civilites.set(civ, civ);
+      });
+    });
+  }
+
 }
