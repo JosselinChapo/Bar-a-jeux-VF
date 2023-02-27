300ca19264e7e0e9ef5de8b21c2941198d0d39b2
audreypen <audreypenazzi@gmail.com>
2023-02-27 15:02:40 +0100

    Accueil

--- front-bar-a-jeux/src/app/app.component.ts
+++ front-bar-a-jeux/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front-bar-a-jeux';
+  title ="Dé qui roule n'amasse pas mousse";
 }
