7ff94ba6efe9b9c095cc0bf25b1f270fc8d92d4b
Maximilien <maximilien.blanc@hotmail.fr>
2023-03-01 17:12:17 +0100

    V4 résa parti Table

--- front-bar-a-jeux/src/app/reservation/reservation.component.ts
+++ front-bar-a-jeux/src/app/reservation/reservation.component.ts
@@ -18,6 +18,17 @@ export class ReservationComponent {
   myForm : FormGroup;
   idTable: number;
 
+  table1: any = "assets/images/reservation/table4rempli.png";
+  table2: any = "assets/images/reservation/table4rempli.png";
+  table3: any = "assets/images/reservation/table6rempli.png";
+  table4: any = "assets/images/reservation/table8rempli.png";
+  table5: any = "assets/images/reservation/table6rempli.png";
+  table6: any = "assets/images/reservation/table4rempli.png";
+  table7: any = "assets/images/reservation/table4rempli.png";
+  table8: any = "assets/images/reservation/table8rempli.png";
+  seletedTable: boolean=false;
+  selectedNumber: number;
+
   constructor(
     private reservationService: ReservationService,
     private _adapter: DateAdapter<any>, 
@@ -41,7 +52,58 @@ export class ReservationComponent {
 
   listTableDisponible(): Array<number> {
     let dateTest: string = this.datepipe.transform(this.formReservation.dateRes, 'yyyy-MM-dd');
-    return this.reservationService.findAllTable(this.formReservation.nbPersonne,dateTest,this.formReservation.heureRes);
+    let tablesDiponibles = this.reservationService.findAllTable(this.formReservation.nbPersonne,dateTest,this.formReservation.heureRes);
+
+    let tablesDiponiblesString=tablesDiponibles.map(String);
+    if (tablesDiponibles.includes(1)){
+      this.table1="assets/images/reservation/table4vide.png";
+    }else{
+      this.table1="assets/images/reservation/table4rempli.png";
+    }
+    
+    if (tablesDiponibles.includes(2)){
+      this.table2="assets/images/reservation/table4vide.png";
+    }else{
+      this.table2="assets/images/reservation/table4rempli.png";
+    }
+    
+    if (tablesDiponibles.includes(3)){
+      this.table3="assets/images/reservation/table6vide.png";
+    }else{
+      this.table3="assets/images/reservation/table6rempli.png";
+    }
+    
+    if (tablesDiponibles.includes(4)){
+      this.table4="assets/images/reservation/table8vide.png";
+    }else{
+      this.table4="assets/images/reservation/table8rempli.png";
+    }
+    
+    if (tablesDiponibles.includes(5)){
+      this.table5="assets/images/reservation/table6vide.png";
+    }else{
+      this.table5="assets/images/reservation/table6rempli.png";
+    }
+    
+    if (tablesDiponibles.includes(6)){
+      this.table6="assets/images/reservation/table4vide.png";
+    }else{
+      this.table6="assets/images/reservation/table4rempli.png";
+    }
+    
+    if (tablesDiponibles.includes(7)){
+      this.table7="assets/images/reservation/table4vide.png";
+    }else{
+      this.table7="assets/images/reservation/table4rempli.png";
+    }
+    
+    if (tablesDiponibles.includes(8)){
+      this.table8="assets/images/reservation/table8vide.png";
+    }else{
+      this.table8="assets/images/reservation/table8rempli.png";
+    }
+
+    return tablesDiponibles;
   }
 
   changeHeure(): void {
@@ -77,7 +139,7 @@ export class ReservationComponent {
     }
     let index: number=-1;
     if (testDates == undefined) {
-      return day !== 0 && day !== 6;
+      return day !== 0 && day !== 1;
     }else {
       return day !== 0 && day !== 1 && testDates.findIndex(testDate => this.datepipe.transform(d, 'yyyy-MM-dd') == this.datepipe.transform(testDate, 'yyyy-MM-dd')) <0;
     };
@@ -111,4 +173,138 @@ export class ReservationComponent {
     this.myFilter;
 
   } 
+
+  changeImage(idImage: number) {
+
+    if (idImage==1 && this.table1!="assets/images/reservation/table4rempli.png"){
+      if (this.table1=="assets/images/reservation/table4select.png"){
+        this.table1="assets/images/reservation/table4vide.png";
+        this.seletedTable=false;
+        this.selectedNumber=undefined;
+      }else {
+        if(this.seletedTable==true){
+          this.changeImage(this.selectedNumber);
+        }
+        this.seletedTable=true;
+        this.selectedNumber=idImage;
+        this.idTable=1;
+        this.table1="assets/images/reservation/table4select.png";
+      }
+    }
+
+    if (idImage==2 && this.table2!="assets/images/reservation/table4rempli.png"){
+      if (this.table2=="assets/images/reservation/table4select.png"){
+        this.table2="assets/images/reservation/table4vide.png";
+        this.seletedTable=false;
+        this.selectedNumber=undefined;
+      }else {
+        if(this.seletedTable==true){
+          this.changeImage(this.selectedNumber);
+        }
+        this.seletedTable=true;
+        this.selectedNumber=idImage;
+        this.idTable=2;
+        this.table2="assets/images/reservation/table4select.png";
+      }
+    }
+
+    if (idImage==3 && this.table3!="assets/images/reservation/table6rempli.png"){
+      if (this.table3=="assets/images/reservation/table6select.png"){
+        this.table3="assets/images/reservation/table6vide.png";
+        this.seletedTable=false;
+        this.selectedNumber=undefined;
+      }else {
+        if(this.seletedTable==true){
+          this.changeImage(this.selectedNumber);
+        }
+        this.seletedTable=true;
+        this.selectedNumber=idImage;
+        this.idTable=3;
+        this.table3="assets/images/reservation/table6select.png";
+      }
+    }
+
+    if (idImage==4 && this.table4!="assets/images/reservation/table8rempli.png"){
+      if (this.table4=="assets/images/reservation/table8select.png"){
+        this.table4="assets/images/reservation/table8vide.png";
+        this.seletedTable=false;
+        this.selectedNumber=undefined;
+      }else {
+        if(this.seletedTable==true){
+          this.changeImage(this.selectedNumber);
+        }
+        this.seletedTable=true;
+        this.selectedNumber=idImage;
+        this.idTable=4;
+        this.table4="assets/images/reservation/table8select.png";
+      }
+    }
+
+    if (idImage==5 && this.table5!="assets/images/reservation/table6rempli.png"){
+      if (this.table5=="assets/images/reservation/table6select.png"){
+        this.table5="assets/images/reservation/table6vide.png";
+        this.seletedTable=false;
+        this.selectedNumber=undefined;
+      }else {
+        if(this.seletedTable==true){
+          this.changeImage(this.selectedNumber);
+        }
+        this.seletedTable=true;
+        this.selectedNumber=idImage;
+        this.idTable=5;
+        this.table5="assets/images/reservation/table6select.png";
+      }
+    }
+
+    if (idImage==6 && this.table6!="assets/images/reservation/table4rempli.png"){
+      if (this.table6=="assets/images/reservation/table4select.png"){
+        this.table6="assets/images/reservation/table4vide.png";
+        this.seletedTable=false;
+        this.selectedNumber=undefined;
+      }else {
+        if(this.seletedTable==true){
+          this.changeImage(this.selectedNumber);
+        }
+        this.seletedTable=true;
+        this.selectedNumber=idImage;
+        this.idTable=6;
+        this.table6="assets/images/reservation/table4select.png";
+      }
+    }
+
+    if (idImage==7 && this.table7!="assets/images/reservation/table4rempli.png"){
+      if (this.table7=="assets/images/reservation/table4select.png"){
+        this.table7="assets/images/reservation/table4vide.png";
+        this.seletedTable=false;
+        this.selectedNumber=undefined;
+      }else {
+        if(this.seletedTable==true){
+          this.changeImage(this.selectedNumber);
+        }
+        this.seletedTable=true;
+        this.selectedNumber=idImage;
+        this.idTable=7;
+        this.table7="assets/images/reservation/table4select.png";
+      }
+    }
+
+    if (idImage==8 && this.table8!="assets/images/reservation/table8rempli.png"){
+      if (this.table8=="assets/images/reservation/table8select.png"){
+        this.table8="assets/images/reservation/table8vide.png";
+        this.seletedTable=false;
+        this.selectedNumber=undefined;
+      }else {
+        if(this.seletedTable==true){
+          this.changeImage(this.selectedNumber);
+        }
+        this.seletedTable=true;
+        this.selectedNumber=idImage;
+        this.idTable=8;
+        this.table8="assets/images/reservation/table8select.png";
+      }
+    }
+
+  }
+
+
 }
